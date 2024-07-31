bb882c8307be4f09ccf29edc2cbfee23fb162aac
developer-tamim <tawsiftamim24@gmail.com>
2024-07-31 16:46:33 +0600

    ...

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar">
         <div class="container-fluid">
           <a class="navbar-brand">Navbar</a>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+          <div class="d-flex">
+            <div class="navbar-brand"><a>Home</a></div>
+            <div class="navbar-brand"><a>About ME</a></div>
+            <div class="navbar-brand"><a>My Work</a></div>
+            <div class="navbar-brand"><a>Contact</a></div>
+          </div>
         </div>
       </nav>
   </div>
