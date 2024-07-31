f32e11e17c8eef9fabf3c015152f3d159a9dafa0
developer-tamim <tawsiftamim24@gmail.com>
2024-07-31 16:23:10 +0600

    bootstrap integrate

---
+++ src/components/layout/HeroSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- Hero 2 - Bootstrap Brain Component -->
+<section class="bsb-hero-2 px-3">
+    <div class="container overflow-hidden">
+      <div class="row gy-3 gy-lg-0 align-items-lg-center justify-content-lg-between">
+        <div class="col-12 col-lg-6 order-1 order-lg-0">
+          <h2 class="display-3 fw-bold mb-3">Art of Design</h2>
+          <p class="fs-4 mb-5">Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.</p>
+          <div class="d-grid gap-2 d-sm-flex">
+            <button type="button" class="btn btn-primary bsb-btn-xl rounded-pill">Free Consultation</button>
+            <button type="button" class="btn btn-outline-primary bsb-btn-xl rounded-pill">Buy Credits</button>
+          </div>
+        </div>
+        <div class="col-12 col-lg-5 text-center">
+          <img class="img-fluid" loading="lazy" src="./assets/img/hero-img-1.jpg" alt="Art of Design" style="-webkit-mask-image: url(./assets/img/hero-img-blob-1.svg); mask-image: url(./assets/img/hero-img-blob-1.svg);">
+        </div>
+      </div>
+    </div>
+  </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeroSection',
+  
+};
+</script>
+
---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+  
+};
+</script>
