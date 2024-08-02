a5db4de81b7ca0b90119db699b6985d762b42289
developer-tamim <tawsiftamim24@gmail.com>
2024-08-02 15:01:02 +0600

    fix nav bar

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -4,10 +4,10 @@
         <div class="container-fluid">
           <a class="navbar-brand">Navbar</a>
           <div class="d-flex">
-            <div class="navbar-brand"><a>Home</a></div>
-            <div class="navbar-brand"><a>About ME</a></div>
-            <div class="navbar-brand"><a>My Work</a></div>
-            <div class="navbar-brand"><a>Contact</a></div>
+            <div class="navbar-brand ms-4"><a>Home</a></div>
+            <div class="navbar-brand ms-4"><a>About ME</a></div>
+            <div class="navbar-brand ms-4"><a>My Work</a></div>
+            <div class="navbar-brand ms-4"><a>Contact</a></div>
           </div>
         </div>
       </nav>
